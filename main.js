245a7af48b102b755f11e010ad9bcd3ef23d5b60
saleh.tarkashvand@gmail.com <ewdwins>
2023-02-23 15:39:59 +0330

    finish slider

---
+++ main.js
@@ -0,0 +1,18 @@
+let slideInedex = 1;
+function setslide(input,index){
+    slideInedex = index;
+    let item = document.querySelector(`#${input}`);
+    let slides = [...document.querySelector(`.slides`).children]
+    slides.forEach((element)=>{
+        element.classList.remove('active');
+    })
+    item.classList.add('active')
+    
+}
+setInterval(()=>{
+    slideInedex+=1;
+    if(slideInedex==4){
+        slideInedex=1;
+    }
+    setslide(`slide${slideInedex}`,slideInedex)
+},4000)
